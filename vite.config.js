dc708bedb400235e5122e0a5216901f2a9b7d0c1
Nagalakshman B S <bsnagalakshman@gmail.com>
2025-08-30 14:30:13 +0530

    add jobs.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/React-Jobs-Project"
+    server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
